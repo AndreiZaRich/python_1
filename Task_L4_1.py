9ffb66af8807f22dab485f3aa51cfefe4529a04f
andreizarich <santizara13a@gmail.com>
2023-06-13 18:10:47 +0300

    Added lambda

--- Task_L4_1.py
+++ Task_L4_1.py
@@ -3,3 +3,11 @@ numbers = [1, 2, 3, 5, 8, 15, 23, 38]
 even_squares = [(num, num ** 2) for num in numbers if num % 2 == 0]
 
 print(even_squares)
+
+# Вариант с lambda
+
+numbers = [1, 2, 3, 5, 8, 15, 23, 38]
+
+even_squares = list(map(lambda num: (num, num ** 2), filter(lambda num: num % 2 == 0, numbers)))
+
+print(even_squares)
